a7d292d85a84f3a45ae6eb9735c331280d5edf2b
pagos38 <kei380402@gmail.com>
2021-08-21 16:34:48 +0900

    Headerデザインの完成

--- src/templates/DashBoardHeader/index.tsx
+++ src/templates/DashBoardHeader/index.tsx
@@ -1,7 +1,8 @@
-import { AppBar, IconButton, Toolbar } from "@material-ui/core";
+import { AppBar, Avatar, IconButton, Toolbar } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
 import { Logo } from "../../components/Logo";
-
+import { SearchBar } from "./SearchBar";
 import useStyles from "./style";
 
 export const DashboardHeader = () => {
@@ -9,12 +10,26 @@ export const DashboardHeader = () => {
 
     return (
         <AppBar elevation={0} color="inherit">
-            <Toolbar>
-                <IconButton>
-                    <MenuIcon />
-                </IconButton>
-                <div className={styles.logo}>
-                    <Logo />
+            <Toolbar className={styles.between}>
+                <div className={styles.flex}>
+                    <IconButton>
+                        <MenuIcon />
+                    </IconButton>
+                    <div className={styles.logo}>
+                        <Logo />
+                    </div>
+                </div>
+
+                <SearchBar />
+
+                <div className={styles.flex}>
+                    <IconButton>
+                        <VideoCallIcon />
+                    </IconButton>
+
+                    <IconButton className={styles.profileIcon}>
+                        <Avatar />
+                    </IconButton>
                 </div>
             </Toolbar>
         </AppBar>
